00d9f9c8bf710766aeebf1c0a671241161e1e794
Tabathapm <tabathapm@gmail.com>
2023-02-10 09:50:32 -0300

    modificación en el index principal

--- proyecto/src/users/index.js
+++ proyecto/src/users/index.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 module.exports.UsersAPI = (app) => {
     router
-        .get('/', UsersController.getUsers)           // http://localhost:3000/api/products/
-        .get('/del/:id', UsersController.deleteUser)  // http://localhost:3000/api/products/del/
-        .get('/upd/:id', UsersController.updateUser)  // http://localhost:3000/api/products/upd/
-        .get('/:id', UsersController.getuser)         // http://localhost:3000/api/products/23
+        .get('/', UsersController.getUsers)           // http://localhost:3000/api/users/
+        .get('/del/:id', UsersController.deleteUser)  // http://localhost:3000/api/users/del/
+        .get('/upd/:id', UsersController.updateUser)  // http://localhost:3000/api/users/upd/
+        .get('/:id', UsersController.getuser)         // http://localhost:3000/api/users/23
         .post('/', UsersController.createUser)
 
 
